8dd7671403cd473e794e6be4cda74389ce511734
fernando.gonzalez <fernando.stoiano@gmail.com>
2019-09-27 21:16:49 -0300

    Create project

---
+++ src/test/kotlin/com/formatter/document/DocumentApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.formatter.document
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class DocumentApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
